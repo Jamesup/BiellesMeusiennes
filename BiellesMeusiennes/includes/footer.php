e4d7dfe63aeb2581965a939a578a66545324ae84
jamesup <jt86@orange.fr>
2016-02-11 22:36:09 +0000

    Dossier bielles

---
+++ BiellesMeusiennes/includes/footer.php
@@ -0,0 +1,16 @@
+
+
+<!-- Tout ce qui va dans le footer -->
+
+
+
+<?php
+
+//fermeture de la connexion de la bdd
+
+unset( $bdd );
+?>
+
+
+</body>
+</html>
---
+++ BiellesMeusiennes/index.php
@@ -0,0 +1,29 @@
+<?php
+
+include('../includes/header.php'); //contient le doctype, et head et le header.
+
+//ouverture de html et body dans header.php 
+
+
+
+
+
+
+
+
+
+// j'ai inclus le formulaire d'inscription ici , mais est-ce vraiment sa place? ou accès avec un lien ??  :D 
+include('../includes/formulaire_inscription.php');
+
+
+
+
+
+
+
+
+
+
+//fermeture de html et body dans footer.php 
+include('../includes/footer.php'); //contient le footer.
+?>
