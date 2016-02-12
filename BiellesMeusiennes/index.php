9ae8d90d89be6d24b020b9cb36afb68b822d7c45
MillHidden <jeremy.hindenoch@gmail.com>
2016-02-12 18:30:39 +0100

    structure

--- BiellesMeusiennes/index.php
+++ BiellesMeusiennes/index.php
@@ -1,29 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
 <?php
 
-include('../includes/header.php'); //contient le doctype, et head et le header.
+include('includes/common/head.php'); //contient le head.
 
-//ouverture de html et body dans header.php 
+include('includes/common/header.php'); //contient le header.
 
-
-
-
-
-
-
-
-
-// j'ai inclus le formulaire d'inscription ici , mais est-ce vraiment sa place? ou accès avec un lien ??  :D 
-include('../includes/formulaire_inscription.php');
-
-
-
-
-
-
-
-
-
-
-//fermeture de html et body dans footer.php 
-include('../includes/footer.php'); //contient le footer.
 ?>
+<body>
+<?php
+
+include('includes/public/formulaire_inscription.php'); //contient le formulaire en lui même 
+?>
+
+
+<?php
+
+include('includes/common/footer.php'); //contient le footer.
+
+?>
+</body>
+</html>
