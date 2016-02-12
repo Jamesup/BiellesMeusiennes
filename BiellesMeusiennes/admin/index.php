0426eb5b000c2cad0ee95c65c1bdf4e6740a5d6d
Jamesup <jt86@orange.fr>
2016-02-12 22:46:30 +0100

    Update index.php
    Modif des chemins d'appel des includes

--- BiellesMeusiennes/admin/index.php
+++ BiellesMeusiennes/admin/index.php
@@ -1,26 +1,26 @@
 <?php
 session_start();
-
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
 <?php
 
-include('includes/common/head.php'); //contient le head.
+include('../includes/common/head.php'); //contient le head.
 
-include('includes/common/header.php'); //contient le header.
+include('../includes/common/header.php'); //contient le header.
 
 ?>
 <body>
 <?php
 
-include('includes/admin/login.php'); //contient le login de la page admin 
+include('../includes/admin/login.php'); //contient le login de la page admin 
 ?>
 
 
 <?php
 
-include('includes/common/footer.php'); //contient le footer.
+include('../includes/common/footer.php'); //contient le footer.
 
 ?>
 </body>
