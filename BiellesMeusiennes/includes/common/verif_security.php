83ec028dbac764de418a4ce96c1298e0f5d7518f
MillHidden <jeremy.hindenoch@gmail.com>
2016-02-14 17:29:21 +0100

    ajout_inscription dans functions.php

--- BiellesMeusiennes/includes/common/verif_security.php
+++ BiellesMeusiennes/includes/common/verif_security.php
@@ -1,5 +1,4 @@
 <?php
-
 /*cette fonction génère un token de session pour le compte administrateur */
 function generer_token() {
 	if(!isset($_SESSION)) { 
@@ -16,17 +15,15 @@ function verif_origin_user () {
 	}
 	if (isset($_SERVER['HTTP_REFERER'])) {
 		/*si l'utilisateur est sur le site admin, il doit avoir une session active qui n'existe qu'après login*/
-		if ($_SERVER['HTTP_REFERER'] == "http://localhost/Biellesmeusiennes-1/admin/") {
+		if ($_SERVER['HTTP_REFERER'] == "http://localhost/BiellesMeusiennes-1/BiellesMeusiennes/admin/") {
 			if (!isset($_GET['token']) || $_GET["token"] != $_SESSION['token']) {
-				die ('jeton de sécurité périmé');
+				throw new Exception ('jeton de sécurité périmé');
 			}
 		}
-		/* on refuse toute connexion qui provient d'un autre site que celui des Bielles Meusiennes public*/
-		else if ($_SERVER['HTTP_REFERER'] != "http://localhost/Biellesmeusiennes-1/") {
-			die ('vous venez de ce site non autorisé : '. $_SERVER['HTTP_REFERER']);
-		}
-		else return true;
+		/* sécurisation failles CSRF. on refuse toute connexion qui provient d'un autre site que celui des Bielles Meusiennes public*/
+		else if (!preg_match("#^http:\/\/localhost\/site%20distant%20autorise\/#", $_SERVER['HTTP_REFERER']) && !preg_match("#hiddenj.jimdo.com#", $_SERVER['HTTP_REFERER'])) {
+			throw new Exception ('vous venez de ce site non autorisé : '. $_SERVER['HTTP_REFERER']);
+		}		
 	}
-}
-	
+}	
 ?>
