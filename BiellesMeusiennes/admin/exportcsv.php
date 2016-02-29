a1075819de1e880873f1639dee496de9fd24de3b
JcPires <djyss@live.fr>
2016-02-29 16:41:59 +0100

    Ajout des en-tetes dans l'export csv

--- BiellesMeusiennes/admin/exportcsv.php
+++ BiellesMeusiennes/admin/exportcsv.php
@@ -1,5 +1,5 @@
 <?php
-include('../includes/common/verif_security.php'); 
+include('../includes/common/verif_security.php');
 
 try {
     verif_origin_user();
@@ -7,9 +7,36 @@ try {
    header('Location: http://localhost/BiellesMeusiennes/BiellesMeusiennes/admin/index.php?message=errortoken&token=' . $_GET['token'] );
     die();
 }
-
 require "../vendor/autoload.php";
 use Core\Configure\Config;
-$owners = Config::QueryBuilder()->findAll('Owners')->execute();
+use Core\Export\DataExporter;
+
+$exposants = Config::QueryBuilder()->findAll('Exposants')->execute();
+
 $csv = new DataExporter('Output/sortie', 'csv');
-$csv->export($membres);
+
+$titles = [
+    "id"=> "id",
+    "firstname"=> "Prenom",
+    "lastname"=> "Nom",
+    "email"=> "Email",
+    "city"=> "Ville",
+    "cp"=> "CP",
+    "country"=> "Pays",
+    "newsletter"=> "Newsletter",
+    "club"=> "Club",
+    "marque" => "Marque",
+    "model" => "Model",
+    "type" => "Type",
+    "motorisation" => "Motorisation",
+    "immat" => "Immatriculation",
+    "date_circu" => "Date 1ere circulation",
+    "infos" => "Infos comp.",
+    "concours1" => "Claude Lorrenzini",
+    "concours2" => "Coupe-Cabriolet",
+    "concours3" => "Jeune -25 ans",
+    "valid"=> "Valide"
+];
+
+array_unshift($exposants, $titles);
+$csv->export($exposants);
